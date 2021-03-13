5d37cb160873d82f6872a2a17bb5b725d386f2ab
Tudor Hulban <prindex2010@gmail.com>
2021-03-13 18:37:27 +0200

    Create main.go

---
+++ 02_Exercises/10_Palindrom/main.go
@@ -0,0 +1,44 @@
+package main
+
+/*
+
+Check if passed string is a palindrome.
+A palindrome is a word, number, phrase, or other
+sequence of characters which reads the same backward as forward,
+such as madam or racecar.
+
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	s1 := "tenet"
+	s2 := "aa"
+
+	fmt.Println(check(s1))
+	fmt.Println(check(s2))
+}
+
+func check(s string) bool {
+	l := len(s)
+
+	if l%2 == 0 {
+		return false
+	}
+
+	half := l / 2
+
+	h1 := s[:half]
+	h2 := s[half+1:]
+
+	// did not use runes and reverse string
+	for i := 0; i < half-1; i++ {
+		if h1[i] != h2[len(h2)-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
