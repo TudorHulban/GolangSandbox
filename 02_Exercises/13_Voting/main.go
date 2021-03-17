58b42d46b8fcae61136081bec57fd67be19e003f
Tudor Hulban <prindex2010@gmail.com>
2021-03-17 13:38:24 +0200

    Create main.go

---
+++ 02_Exercises/13_Voting/main.go
@@ -0,0 +1,91 @@
+package main
+
+// "aaabbbcddd" a -> 3 b -> 3 c -1 d -> 3
+
+// => Winner is : a  (max votes, single person)
+// => loser is : b (min votes, single person)
+// => no winner
+
+// Length of the string is N (i.e. n different votes)
+// "aaaabbbcddd" -> Winner is a, Loser is c
+// "aaabbbcddd" -> No winner
+// "aaabcd" -> Winner is a
+// "abcd" -> No Winner
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := "abcd" // every letter is a vote. if maximum number votes twice, no winner
+
+	m := identifyEl(s)
+	fmt.Println(wins(m))
+}
+
+func identifyEl(s string) map[rune]int {
+	result := make(map[rune]int)
+
+	for _, v := range s {
+		if _, exists := result[v]; exists {
+			result[v]++
+			continue
+		}
+
+		result[v] = 1
+	}
+
+	return result
+}
+
+// returns winner, loser, error
+func wins(m map[rune]int) (string, string, error) {
+	if len(m) == 0 {
+		return "", "", errors.New("no votes")
+	}
+
+	var a []int
+	for _, v := range m {
+		a = append(a, v)
+	}
+
+	if len(a) == 1 {
+		return "", "", errors.New("no winner or loser")
+	}
+
+	sort.Ints(a)
+
+	// check if occurences are the same
+	if a[0] == a[len(a)-1] {
+		return "", "", errors.New("no winner or loser")
+	}
+
+	// find runes in map
+	var winner, loser rune
+
+	for k, r := range m {
+		if r == a[len(a)-1] {
+			winner = k
+		}
+
+		if r == a[0] {
+			loser = k
+		}
+
+		if winner != 0 && loser != 0 {
+			break
+		}
+	}
+
+	if a[len(a)-1] == a[len(a)-2] {
+		return "", string(loser), errors.New("but no winner")
+	}
+
+	if a[0] == a[1] {
+		return string(winner), "", errors.New("and no loser")
+	}
+
+	return string(winner), string(loser), nil
+}
