49fc513e3f04a32b573d5c194a9d44df54f5608b
Tudor Hulban <prindex2010@gmail.com>
2021-03-29 17:38:05 +0300

    leaving optimal solution

--- 02_Exercises/13_Voting/main.go
+++ 02_Exercises/13_Voting/main.go
@@ -19,12 +19,6 @@ import (
 )
 
 // MapData Exported for future use.
-type MapData struct {
-	Key   rune
-	Value int
-}
-
-// MapData Exported for future use.
 type MapString struct {
 	Key   string
 	Value int
@@ -33,11 +27,8 @@ type MapString struct {
 func main() {
 	s := "abcdadba" // every letter is a vote. if maximum number votes twice, no winner
 
-	m1 := identifyEl(s)
-	fmt.Println(wins(m1))
-
 	m2 := identifyString(s)
-	fmt.Println(winsString(orderMapString(m2)))
+	fmt.Println(wins(orderMapString(m2)))
 }
 
 func identifyString(s string) map[string]int {
@@ -78,7 +69,7 @@ func orderMapString(m map[string]int) []MapString {
 	return result
 }
 
-func winsString(m []MapString) (string, string, error) {
+func wins(m []MapString) (string, string, error) {
 	if len(m) == 0 {
 		return "", "", errors.New("no votes")
 	}
@@ -100,90 +91,4 @@ func winsString(m []MapString) (string, string, error) {
 	}
 
 	return m[len(m)-1].Key, m[0].Key, nil
-}
-
-func identifyEl(s string) map[rune]int {
-	result := make(map[rune]int)
-
-	for _, v := range s {
-		if _, exists := result[v]; exists {
-			result[v]++
-			continue
-		}
-
-		result[v] = 1
-	}
-
-	return result
-}
-
-func orderMapData(m map[rune]int) []MapData {
-	if len(m) == 0 {
-		return []MapData{}
-	}
-
-	var result []MapData
-
-	for k, v := range m {
-		result = append(result, MapData{
-			Key:   k,
-			Value: v,
-		})
-	}
-
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].Value < result[j].Value
-	})
-
-	return result
-}
-
-// returns winner, loser, error
-func wins(m map[rune]int) (string, string, error) {
-	if len(m) == 0 {
-		return "", "", errors.New("no votes")
-	}
-
-	var a []int
-	for _, v := range m {
-		a = append(a, v)
-	}
-
-	if len(a) == 1 {
-		return "", "", errors.New("no winner or loser")
-	}
-
-	sort.Ints(a)
-
-	// check if occurences are the same
-	if a[0] == a[len(a)-1] {
-		return "", "", errors.New("no winner or loser")
-	}
-
-	// find runes in map
-	var winner, loser rune
-
-	for k, r := range m {
-		if r == a[len(a)-1] {
-			winner = k
-		}
-
-		if r == a[0] {
-			loser = k
-		}
-
-		if winner != 0 && loser != 0 {
-			break
-		}
-	}
-
-	if a[len(a)-1] == a[len(a)-2] {
-		return "", string(loser), errors.New("but no winner")
-	}
-
-	if a[0] == a[1] {
-		return string(winner), "", errors.New("and no loser")
-	}
-
-	return string(winner), string(loser), nil
 }
--- 02_Exercises/13_Voting/t_test.go
+++ 02_Exercises/13_Voting/t_test.go
@@ -6,15 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLogic(t *testing.T) {
-	m := identifyEl("saa")
-	s := orderMapData(m)
-
-	require.Len(t, s, 2)
-	require.Equal(t, s[0].Value, 1)
-	require.Equal(t, s[1].Value, 2)
-}
-
 func TestString(t *testing.T) {
 	m := identifyString("saa")
 	s := orderMapString(m)
