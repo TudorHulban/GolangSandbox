f37a3ef1c07fdf2250d299b445d3e91079878269
Tudor Hulban <prindex2010@gmail.com>
2021-07-13 13:39:21 +0300

    Update main.go

--- 03_SortingFiltering/02_SortSliceStruct/main.go
+++ 03_SortingFiltering/02_SortSliceStruct/main.go
@@ -17,10 +17,17 @@ func sortAsc(tasks []task) {
 	})
 }
 
+func sortName(tasks []task) {
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i].name < tasks[j].name
+	})
+}
+
 func main() {
-	tasks := []task{{id: 11, name: "t11"}, {id: 2, name: "t2"}, {id: 3, name: "t3"}}
+	tasks := []task{{id: 11, name: "t11"}, {id: 2, name: "t2"}, {id: 3, name: "t4"}, {id: 3, name: "t3"}}
 	log.Println("Raw:", tasks)
 
 	sortAsc(tasks)
+	sortName(tasks)
 	log.Println("Processed:", tasks)
 }
